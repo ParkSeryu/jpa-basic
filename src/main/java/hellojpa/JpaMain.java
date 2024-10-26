8de0417a7c9ad67ba1ce72d71f5c8f2c79342aef
parkSeryu <pjh971212@gmail.com>
2024-10-26 16:58:37 +0900

    프록시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,39 +1,61 @@
 package hellojpa;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
-import java.util.List;
+import org.hibernate.Hibernate;
 
 public class JpaMain {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
 
-        try {
+    try {
 
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
+      Member member1 = new Member();
+      member1.setUsername("hello");
+      em.persist(member1);
 
-            em.persist(member);
+      Member member2 = new Member();
+      member2.setUsername("member2");
+      em.persist(member2);
 
-            em.flush();
-            em.clear();
+      em.flush();
+      em.clear();
 
-            tx.commit();
-        } catch (Exception e) {
-            System.out.println("e" + e.toString());
-            tx.rollback();
-        } finally {
-            em.close();
-        }
+      Member refMember = em.getReference(Member.class, member1.getId());
+      System.out.println("refMember = " + refMember.getClass()); // Proxy
+      Hibernate.initialize(refMember); // 강제초기화
 
-        emf.close();
+
+      System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+
+      tx.commit();
+    } catch (Exception e) {
+      System.out.println("e" + e.toString());
+      tx.rollback();
+    } finally {
+      em.close();
     }
+
+    emf.close();
+  }
+
+  private static void printMember(Member member) {
+    System.out.println("member.getUsername() = " + member.getUsername());
+
+  }
+
+  private static void printMemberAndTeam(Member member) {
+    String username = member.getUsername();
+    System.out.println("username = " + username);
+
+    Team team = member.getTeam();
+    System.out.println("team = " + team.getName());
+
+
+  }
 
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -17,62 +17,58 @@ import java.util.List;
 
 @Entity
 public class Member extends BaseEntity {
-    @Id
-    @GeneratedValue
-    @Column(name = "MEMBER_ID")
-    private Long id;
 
-    @Column(name = "USERNAME")
-    private String username;
+  @Id
+  @GeneratedValue
+  @Column(name = "MEMBER_ID")
+  private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
-    private Team team;
+  @Column(name = "USERNAME")
+  private String username;
 
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
+  @ManyToOne
+  @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+  private Team team;
 
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
+  @OneToMany(mappedBy = "member")
+  private List<MemberProduct> memberProducts = new ArrayList<>();
 
 
+  public void setTeam(Team team) {
+    this.team = team;
+  }
 
-    public void setTeam(Team team) {
-        this.team = team;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public String getUsername() {
-        return username;
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
+  public Team getTeam() {
+    return team;
+  }
 
 //    public void changeTeam(Team team) {
 //        this.team = team;
 //        team.getMembers().add(this);
 //    }
 
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", team=" + team +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Member{" +
+        "id=" + id +
+        ", username='" + username + '\'' +
+        ", team=" + team +
+        '}';
+  }
 }
